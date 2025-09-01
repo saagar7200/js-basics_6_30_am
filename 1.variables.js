6c5dcc41a97d4b94c1f14eac6c7875d97cb91cfb
Sagar Bhandari <rsaagar7200@gmail.com>
2025-09-01 08:59:46 +0545

    js varibles

---
+++ 1.variables.js
@@ -0,0 +1,84 @@
+// console.log("hello world");
+
+// console.log(10 + 20)
+//* methods to declare variables
+// var let const
+var a  = 29
+
+a =40
+
+// var a =30
+a = 'Hello'
+
+console.log(a) 
+
+
+let b  = 40
+
+
+b = 40
+console.log(b)
+
+// let b =30
+
+const c = 29
+// c = 34
+console.log(c)
+
+// single threaded
+// interpreted
+// dynamic typed
+
+
+let d  , e
+
+
+// data types 
+// primitive
+//* number
+let f = 30
+let g = 30.67
+
+// * string ('' , "")
+let h = "Good morning"
+let i = 'hello'
+
+// * template literals (``)
+
+const str = `
+
+
+dsfsd
+
+
+
+sdf`
+
+const user = 'John'
+
+let greet = `hello, ${user} good morning`
+
+// console.log(greet)
+
+let n;
+
+// * boolean
+let j = true
+let k = false
+
+console.log(n)
+
+// undefined
+n = 20
+console.log(n)
+let l = undefined
+// null
+let m = null
+
+console.log(m)
+
+
+
+// non 
+// array
+// object
