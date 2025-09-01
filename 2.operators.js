2289c99c18ee850c4a3aad237057dbd33818ece3
Sagar Bhandari <rsaagar7200@gmail.com>
2025-09-01 09:00:40 +0545

    operators

---
+++ 2.operators.js
@@ -0,0 +1,110 @@
+// 
+
+// Arithmetic
+// + , - /, %(modulus), * , ** (exponential) ..
+console.log(10 + 20)
+let a = 41
+let b = 20
+// console.log(a + b)
+let sum  = a + b
+// console.log(sum)
+// console.log(2 ** 2)
+
+// assignment
+//  = , += , -= /= ...
+// a = a + b 
+a += b
+// console.log(a)
+
+// comparision
+//  == , === , < , > , != ,!==
+
+let c = 10
+let d = 10
+// console.log(c != d)
+// console.log(c <= d)
+
+
+// logical
+// AND (&&)
+// OR ( || )
+// NOT (!)
+
+// console.log(true  && true)  // true
+// console.log(true  && false)  // false
+// console.log(false  && true)  // false
+
+const isUser = false
+
+const isStudent = false
+
+let isStuAndUser = isUser && isStudent
+// console.log(isStuAndUser)
+
+// OR (||)
+
+// a or b -> 
+
+
+ isStuAndUser = isUser || isStudent
+// console.log(isStuAndUser)
+// console.log(false  && false)
+// console.log(false  && false)
+
+// NOT (!)
+
+// console.log(!!isUser)
+
+
+// typeof
+let x;
+console.log(typeof x === 'undefined')
+
+let e = 20
+let f ='hfg'
+console.log(e - f)
+
+// Number()
+// Boolean()
+// String()
+
+// console.log(Number('hjs'))
+
+// ternary operator  ? :
+// condition ? statement 1 : statement 2
+
+
+const user = isUser ? 'user' : 'student'
+
+console.log(user)
+
+// bitwise
+// and (&)  or (|) xor (^)  // sift >>
+
+console.log(3 | 2)
+// 010
+// 001
+// 000 -> 0
+
+// 011
+// 010
+// 011 - > 3
+
+
+
+// 
+
+
+
+// console.log(typeof null === 'object')
+
+
+
+//* naming conventions
+// snake case  -> let snakecase -> snake_case
+// camel case -> let camelcase -> camelCase
+// pascal case ->              > PascalCase
+
+
+// 
+// let a b = 'sad'
