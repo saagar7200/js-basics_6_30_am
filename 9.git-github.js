75eb6f64e41a4903090ba4fcf02455128a8f3606
Sagar Bhandari <rsaagar7200@gmail.com>
2025-09-02 08:43:33 +0545

    ...

--- 9.git-github.js
+++ 9.git-github.js
@@ -26,7 +26,7 @@
 // * git push origin <branch_name>  -> push local repo to remote repo
 // * git pull origin <branch_name>
 
-//* brnach 
+//! brnach 
 //* git branch    -> list all available branches
 //* git branch <branch_name>  -> create new branch
 //* git checkout <branch_name>  -> switch branch
